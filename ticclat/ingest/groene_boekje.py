ab43c2056afdd78a1f9bd8d898e162eb80f47680
E. G. Patrick Bos <egpbos@gmail.com>
2019-02-19 15:42:52 +0100

    restructures groene_boekje.py for reuse

--- ticclat/ingest/groene_boekje.py
+++ ticclat/ingest/groene_boekje.py
@@ -8,46 +8,35 @@ import sqlalchemy_utils
 import ticclat.dbutils
 import ticclat.ticclat_schema as schema
 
-# basic config
-db_name = 'ticclat'
-GB_basepath = "/Users/pbos/projects/ticclat/data/GB/"
-envvars_path = '/Users/pbos/projects/ticclat/ticclat/notebooks/ticclat_db_ingestion/ENVVARS.txt'
-
-
-# Read information to connect to the database and put it in environment variables
-with open(envvars_path) as f:
-    for line in f:
-        parts = line.split('=')
-        if len(parts) == 2:
-            os.environ[parts[0]] = parts[1].strip()
-
-
-engine = sqlalchemy.create_engine("mysql://{}:{}@localhost/{}".format(os.environ['user'], 
-                                                                      os.environ['password'], 
-                                                                      os.environ['dbname']))
-if not sqlalchemy_utils.database_exists(engine.url):
-    sqlalchemy_utils.create_database(engine.url)
-
-Session = sqlalchemy.orm.sessionmaker(bind=engine)
-
-# create tables
-schema.Base.metadata.create_all(engine)
-
-
-GB1914_path = GB_basepath + "1914/22722-8.txt"
-GB1995_path = GB_basepath + "1995-2005/1995/GB95_002.csv"
-GB2005_path = GB_basepath + "1995-2005/2005/GB05_002.csv"
 
 ######################
 # Groene Boekje 1995 #
 ######################
+def load_GB95(path):
+    df = pd.read_csv(path, sep=';', escapechar='\\',
+                     names=["word", "syllables", "see also", "disambiguation",
+                            "grammatical tag", "article",
+                            "plural/past/attrib", "plural/past/attrib syllables",
+                            "diminu/compara/past plural", "diminu/compara/past plural syllables",
+                            "past perfect/superla", "past perfect/superla syllables"],
+                     encoding='utf8') # encoding necessary for later loading into sqlalchemy!
 
-df_GB1995 = pd.read_csv(GB1995_path, sep=';', names=["word", "syllables", "see also", "disambiguation",
-                                                     "grammatical tag", "article",
-                                                     "plural/past/attrib", "plural/past/attrib syllables",
-                                                     "diminu/compara/past plural", "diminu/compara/past plural syllables",
-                                                     "past perfect/superla", "past perfect/superla syllables"],
-                        encoding='utf8') # encoding necessary for later loading into sqlalchemy!
+    df = df.where(df != "Ili/as", other=None)
+
+    return df
+
+
+# note that these are regex formatted, i.e. with special characters escaped
+diacritic_markers = {'@`': '\u0300',    # accent grave
+                     "@\\'": '\u0301',  # accent aigu
+                     '@\\"': '\u0308', # trema
+                     '@\+': '\u0327',   # cedilla
+                     '@\^': '\u0302',   # accent circumflex
+                     '@=': '\u0303',    # tilde
+                     '@@': "'",         # apostrophe (not actually a diacritic)
+                     '@2': '\u2082',    # subscript 2
+                     '@n': '\u0308n'    # trema followed by n
+                    }
 
 
 def nd_any(*args):
@@ -75,35 +64,59 @@ def clean_wordform_df(wordform_df):
     return wordform_df
 
 
-wordform_df = pd.concat((df_GB1995["word"],
-                         df_GB1995["plural/past/attrib"],
-                         df_GB1995["diminu/compara/past plural"],
-                         df_GB1995["past perfect/superla"]))\
-                .dropna()
-has_comma = wordform_df.str.contains(', ')
-wordform_df = pd.concat((wordform_df[~has_comma],) + tuple(pd.Series(row.split(', ')) for row in wordform_df[has_comma]))
+def create_GB95_wordform_df(df_GB1995):
+    wordform_df = pd.concat((df_GB1995["word"],
+                             df_GB1995["plural/past/attrib"],
+                             df_GB1995["diminu/compara/past plural"],
+                             df_GB1995["past perfect/superla"]))\
+                    .dropna()
+    has_comma = wordform_df.str.contains(', ')
+    wordform_df = pd.concat((wordform_df[~has_comma],) + tuple(pd.Series(row.split(', ')) for row in wordform_df[has_comma]))
 
-wordform_clean_df = clean_wordform_df(wordform_df)
+    wordform_clean_df = clean_wordform_df(wordform_df)
 
+    for marker, umarker in diacritic_markers.items():
+        wordform_clean_df = wordform_clean_df.str.replace(marker, umarker)
 
-# ### Normalizing diacritics
-# note that these are regex formatted, i.e. with special characters escaped
-diacritic_markers = {'@`': '\u0300',    # accent grave
-                     "@\\'": '\u0301',  # accent aigu
-                     '@\\\\': '\u0308', # trema
-                     '@\+': '\u0327',   # cedilla
-                     '@\^': '\u0302',   # accent circumflex
-                     '@=': '\u0303',    # tilde
-                     '@@': "'",         # apostrophe (not actually a diacritic)
-                     '@2': '\u2082',    # subscript 2
-                     '@n': '\u0308n'    # trema followed by n
-                    }
+    return wordform_df
 
 
-for marker, umarker in diacritic_markers.items():
-    wordform_clean_df = wordform_clean_df.str.replace(marker, umarker)
+if __name__ == '__main__':
+    # basic config
+    db_name = 'ticclat'
+    GB_basepath = "/Users/pbos/projects/ticclat/data/GB/"
+    envvars_path = '/Users/pbos/projects/ticclat/ticclat/notebooks/ticclat_db_ingestion/ENVVARS.txt'
+
+    # paths
+    GB1914_path = GB_basepath + "1914/22722-8.txt"
+    GB1995_path = GB_basepath + "1995-2005/1995/GB95_002.csv"
+    GB2005_path = GB_basepath + "1995-2005/2005/GB05_002.csv"
 
 
-# # Load Groene Boekje DataFrames into TICCLAT database
-with ticclat.dbutils.session_scope(Session) as session:
-    ticclat.dbutils.add_lexicon(session, "Groene Boekje 1995", True, wordform_clean_df.str.encode('utf8').to_frame(name='wordform'))
+    # Read information to connect to the database and put it in environment variables
+    with open(envvars_path) as f:
+        for line in f:
+            parts = line.split('=')
+            if len(parts) == 2:
+                os.environ[parts[0]] = parts[1].strip()
+
+
+    engine = sqlalchemy.create_engine("mysql://{}:{}@localhost/{}".format(os.environ['user'], 
+                                                                        os.environ['password'], 
+                                                                        os.environ['dbname']))
+    if not sqlalchemy_utils.database_exists(engine.url):
+        sqlalchemy_utils.create_database(engine.url)
+
+    Session = sqlalchemy.orm.sessionmaker(bind=engine)
+
+    # create tables
+    schema.Base.metadata.create_all(engine)
+
+
+    # wordforms
+    wordform_clean_df = create_GB95_wordform_df(load_GB95(GB1995_path))
+
+    # # Load Groene Boekje wordforms into TICCLAT database
+    with ticclat.dbutils.session_scope(Session) as session:
+        ticclat.dbutils.add_lexicon(session, "Groene Boekje 1995", True, wordform_clean_df.str.encode('utf8').to_frame(name='wordform'))
+
