3dac52170afe7ee20f230be64c8c82ebd7beb994
Janneke van der Zwaan <j.vanderzwaan@esciencecenter.nl>
2019-09-13 18:08:17 +0200

    First delete wordform links then wordforms for lexicon
    Fixes StaleDataError that occured when running the tests in mysql
    instead of sqlite.

--- ticclat/dev_utils.py
+++ ticclat/dev_utils.py
@@ -6,13 +6,11 @@ logger = logging.getLogger(__name__)
 
 
 def delete_lexicon(session, lexicon_id):
-    engine = session.get_bind()
     lexicon = session.query(Lexicon).get(lexicon_id)
 
     # delete relevant related objects
-    lexicon.lexicon_wordforms = []
     lexicon.lexicon_wordform_links = []
+    lexicon.lexicon_wordforms = []
 
     # delete lexicon
-    d = Lexicon.__table__.delete().where(Lexicon.lexicon_id == lexicon_id)
-    engine.execute(d)
+    session.delete(lexicon)
