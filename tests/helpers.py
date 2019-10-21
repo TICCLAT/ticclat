c9047ef3fa8eee72377cc6549f6d85e5089f5b7e
Tom Klaver <tomklav@gmail.com>
2019-10-21 16:26:04 +0200

    adds test for mock data inserts

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,3 +1,10 @@
+from pathlib import Path
 
-def load_test_db_data():
-    pass
+import pandas
+
+
+def load_test_db_data(dbsession):
+    files = (Path(__file__).parent/'db_data').glob('./*.tsv')
+    for file in files:
+        df = pandas.read_csv(file, sep='\t')
+        df.to_sql(file.stem, if_exists='append', index=False, con=dbsession.bind)
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,2 +1,25 @@
+from pathlib import Path
+
+from tests.helpers import load_test_db_data
+
+
 def test_load_db_data(dbsession):
-    assert 1==2
+    # load the data
+    load_test_db_data(dbsession)
+
+    # assert that numbers of rows correspond to number of rows in tsv files
+    files = (Path(__file__).parent / 'db_data').glob('./*.tsv')
+    for file in files:
+        r = dbsession.execute(f"SELECT * FROM {file.stem}")
+        with open(file) as f:
+            num_lines = sum(1 for _ in f)
+            # assert DB contains num of lines + 1 (head with column names)
+            assert num_lines == r.rowcount + 1
+
+
+def test_db_data_no_load(dbsession):
+    # assert that DB is empty without load_test_db_data call
+    files = (Path(__file__).parent / 'db_data').glob('./*.tsv')
+    for file in files:
+        r = dbsession.execute(f"SELECT * FROM {file.stem}")
+        assert r.rowcount == 0
